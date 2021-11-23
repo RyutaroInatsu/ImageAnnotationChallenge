4b86ad3980f636758a2f4477b9874e55434e3359
Ryutaro_Inatsu_Win <ryu.tennis.kh.roxas.l@gmail.com>
2021-11-23 10:30:11 +0900

    Changed to apply data_augmentation to images

--- src/ImgTagDataset.py
+++ src/ImgTagDataset.py
@@ -9,7 +9,7 @@ from PIL import Image
 from sklearn.preprocessing import MultiLabelBinarizer
 from sklearn.model_selection import train_test_split
 from torch.utils.data import DataLoader, Dataset
-from torchvision import transforms
+from albumentations.pytorch import ToTensorV2
 
 
 class ImgTagDataset(Dataset):
@@ -20,13 +20,14 @@ class ImgTagDataset(Dataset):
     ----------------
     df : pd.DataFrame
         Pandas DataFrame object.
-        it must be contained "directory", "filename", and one-hot encoded columns.
+        it must be contained "directory", "filename", and multi-binarized encoded columns.
 
     root_path: str
         root path of Datasets
     """
 
-    def __init__(self, root_path, transform):
+    def __init__(self, df: pd.DataFrame, root_path, transform):
+        self.df = df
         self.root_path = root_path
         self.transform = transform
 
@@ -39,6 +40,7 @@ class ImgTagDataset(Dataset):
             os.path.join(self.root_path, d["directory"], d["filename"])
         ).convert("RGB")
         image = self.transform(image=np.squeeze(image))["image"]
+        image = np.clip(image, 0, 1)
         label = torch.tensor(d[2:].tolist(), dtype=torch.float32)
         return image, label
 
@@ -50,8 +52,8 @@ class ImgTagDataModule(pl.LightningDataModule):
         test_root_dir: str,
         batch_size=32,
         img_size=224,
-        mean=(0.485, 0.456, 0.406),
-        std=(0.229, 0.224, 0.225),
+        mean=(0.5, 0.5, 0.5),
+        std=(0.5, 0.5, 0.5),
     ):
 
         super().__init__()
@@ -64,28 +66,29 @@ class ImgTagDataModule(pl.LightningDataModule):
         self.test_ds = None
 
         # pre-processing
-        self.train_transforms = alb.Compose(
+        self.train_augmentation = alb.Compose(
             [
                 alb.RandomResizedCrop(
                     width=img_size, height=img_size, scale=(0.5, 1.0)
                 ),
+                alb.ShiftScaleRotate(),
                 alb.RandomBrightnessContrast(
                     brightness_limit=0.1, contrast_limit=0.2, p=0.5
                 ),
                 alb.ImageCompression(quality_lower=90, quality_upper=100, p=0.5),
                 alb.GaussianBlur(blur_limit=(1, 3)),
-                alb.CLAHE(clip_limit=6.0, tile_grid_size=(8, 8), p=1),
+                # alb.CLAHE(clip_limit=6.0, tile_grid_size=(8, 8), p=1),
                 alb.HorizontalFlip(),
                 alb.Normalize(mean, std),
+                ToTensorV2(),
             ]
         )
 
-        self.val_test_transforms = transforms.Compose(
+        self.val_augmentation = alb.Compose(
             [
-                transforms.Resize(img_size),
-                transforms.CenterCrop(img_size),
-                transforms.ToTensor(),
-                transforms.Normalize(mean=mean, std=std),
+                alb.Resize(height=img_size, width=img_size),
+                alb.Normalize(mean=mean, std=std),
+                ToTensorV2(),
             ]
         )
 
@@ -96,32 +99,39 @@ class ImgTagDataModule(pl.LightningDataModule):
         train_df, val_df = train_test_split(df, test_size=0.2)
         return train_df, val_df
 
+    def binarize_df(self, label_path):
+        df = pd.read_csv(label_path)
+        df = df.dropna(axis=1, how="all")  # save memory and process usage
+        df = df.fillna("None")  # to avoid error
+
+        mlb = MultiLabelBinarizer()
+        result = mlb.fit_transform(df.drop(columns=["directory", "filename"]).values)  # drop not tagging cols
+
+        bin_df = pd.DataFrame(result, columns=mlb.classes_).drop("None", axis=1)  # drop non-useless col.
+        return df.drop(df.columns[2:], axis=1).join(bin_df)
+
     # Trainer.fit()ではtrain/valのDatasetを、Trainer.test()ではtestのDatasetを生成
     def setup(self, stage=None):
         if stage == 'fit' or stage is None:
-            df = pd.read_csv(os.path.join(self.train_val_root_dir, "label.csv"))
-            df = df.dropna(axis=1, how="all")  # save memory and process usage
-            df = df.fillna("None")  # to avoid error
-
-            mlb = MultiLabelBinarizer()
-            result = mlb.fit_transform(df.drop(columns=["directory", "filename"]).values)  # drop not tagging cols
-
-            bin_df = pd.DataFrame(result, columns=mlb.classes_).drop("None", axis=1)  # drop non-useless col.
-            bin_df = df.drop(df.columns[2:], axis=1).join(bin_df)
-
+            bin_df = self.binarize_df(os.path.join(self.train_val_root_dir, "label.csv"))
             train_df, val_df = self.split_train_val_df(bin_df)
-            self.train_ds = ImgTagDataset(train_df, self.train_val_root_dir, self.train_transforms)
-            self.val_ds = ImgTagDataset(val_df, self.train_val_root_dir, self.val_test_transforms)
+
+            self.train_ds = ImgTagDataset(train_df, self.train_val_root_dir, self.train_augmentation)
+            self.val_ds = ImgTagDataset(val_df, self.train_val_root_dir, self.val_augmentation)
 
         if stage == 'test' or stage is None:
-            test_df = pd.read_csv(os.path.join(self.test_root_dir, "label.csv"))
-            self.test_ds = ImgTagDataset(test_df, self.test_root_dir, self.val_test_transforms)
+            bin_test_df = self.binarize_df(os.path.join(self.test_root_dir, "label.csv"))
+            self.test_ds = ImgTagDataset(bin_test_df, self.test_root_dir, self.val_augmentation)
+
+    def get_loader(self, phase):
+        return
 
     def train_dataloader(self):
-        return DataLoader(self.train_ds, batch_size=self.batch_size, shuffle=True, num_workers=8, pin_memory=True)
+        # (Known issue) In windows, num_workers > 0 makes stuck in validation checking.
+        return DataLoader(self.train_ds, batch_size=self.batch_size, shuffle=True, num_workers=0)
 
     def val_dataloader(self):
-        return DataLoader(self.val_ds, batch_size=self.batch_size, num_workers=8, pin_memory=True)
+        return DataLoader(self.val_ds, batch_size=self.batch_size, num_workers=0)
 
     def test_dataloader(self):
-        return DataLoader(self.test_ds, batch_size=self.batch_size, num_workers=8, pin_memory=True)
+        return DataLoader(self.test_ds, batch_size=self.batch_size, num_workers=0)
