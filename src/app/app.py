faa65daab4f3b298bfac2a9fb21423941d8d724e
Ryutaro_Inatsu_Win <ryu.tennis.kh.roxas.l@gmail.com>
2021-11-30 17:23:47 +0900

    add error handling when a large file uploaded

--- src/app/app.py
+++ src/app/app.py
@@ -1,8 +1,11 @@
+import werkzeug
+
 from flask import Flask, redirect, render_template, request, jsonify
 from base64 import b64encode
 from inference import get_prediction
 
 app = Flask(__name__)
+app.config['MAX_CONTENT_LENGTH'] = 10 * 1024 * 1024
 
 
 @app.route("/", methods=["GET", "POST"])
@@ -39,6 +42,11 @@ def api_predict():
     predict_class = get_prediction(image_byte=image_byte)
 
     return jsonify({'predict_class': predict_class})
+
+
+@app.errorhandler(werkzeug.exceptions.RequestEntityTooLarge)
+def handle_over_file_size(error):
+    return 'The uploaded image size was larger than 10MB.', 413
 
 
 if __name__ == "__main__":
