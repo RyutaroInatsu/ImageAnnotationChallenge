42b9f3e1814e25dcafad8e08db01e794df1a417d
Ryutaro_Inatsu_Win <ryu.tennis.kh.roxas.l@gmail.com>
2021-12-22 11:06:35 +0900

    fix augmentation method

--- src/app/commons.py
+++ src/app/commons.py
@@ -1,12 +1,14 @@
 import io
 
-import albumentations as alb
-import numpy as np
+# import albumentations as alb
+# import numpy as np
 import torch
-from albumentations.pytorch import ToTensorV2
-from imgtag.ImgTagLitModule import BeiTNet
+
+# from albumentations.pytorch import ToTensorV2
 from PIL import Image
-from transformers import BeitModel
+from transformers import BeitFeatureExtractor, BeitModel
+
+from imgtag.ImgTagLitModule import BeiTNet
 
 IMG_SIZE = 224
 
@@ -14,7 +16,7 @@ IMG_SIZE = 224
 def get_model(class_num):
     # set up model
     beit_model = BeitModel.from_pretrained(
-        'microsoft/beit-base-patch16-224-pt22k-ft22k'
+        "microsoft/beit-base-patch16-224-pt22k-ft22k"
     )
     model = BeiTNet(beit_model, class_num)
     model.load_state_dict(torch.load("./static/screw_beit_model.pt"))
@@ -23,14 +25,20 @@ def get_model(class_num):
 
 
 def transform_image(image_bytes):
-    augmentation = alb.Compose(
-        [
-            alb.Resize(height=IMG_SIZE, width=IMG_SIZE),
-            alb.Normalize(mean=(0.5, 0.5, 0.5), std=(0.5, 0.5, 0.5)),
-            ToTensorV2(),
-        ]
+    beit_feature_extractor = BeitFeatureExtractor.from_pretrained(
+        "microsoft/beit-base-patch16-224-pt22k-ft22k"
     )
+    # augmentation = alb.Compose(
+    #     [
+    #         alb.Resize(height=IMG_SIZE, width=IMG_SIZE),
+    #         # alb.Normalize(mean=(0.5, 0.5, 0.5), std=(0.5, 0.5, 0.5)),
+    #         ToTensorV2(),
+    #     ]
+    # )
     image = Image.open(io.BytesIO(image_bytes))
-    image = augmentation(image=np.squeeze(image))["image"]
-    image = np.clip(image, 0, 1)
+    # image = augmentation(image=np.squeeze(image))["image"]
+    image = beit_feature_extractor(images=image, return_tensors="pt")[
+        "pixel_values"
+    ].squeeze(0)
+    # image = np.clip(image, 0, 1)
     return image.unsqueeze(0)
